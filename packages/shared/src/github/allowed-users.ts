53e5dd954db2c663ff5b4cb3e4ae6c13538456f3
Brace Sproul <braceasproul@gmail.com>
2025-08-21 11:40:36 -0700

    fix: Make the allowed users list an env var (#789)
    * fix: Make the allowed users list an env var
    
    * cr

--- packages/shared/src/github/allowed-users.ts
+++ packages/shared/src/github/allowed-users.ts
@@ -1,69 +1,31 @@
-export const ALLOWED_USERS = [
-  "agola11",
-  "akira",
-  "aliyanishfaq",
-  "andrewnguonly",
-  "angus-langchain",
-  "bracesproul",
-  "ArthurLangChain",
-  "baskaryan",
-  "bvs-langchain",
-  "catherine-langchain",
-  "ccurme",
-  "crystalro0",
-  "dqbd",
-  "emily-langchain",
-  "eric-langchain",
-  "EugeneJinXin",
-  "eyurtsev",
-  "gladwig2",
-  "hari-dhanushkodi",
-  "hinthornw",
-  "hntrl",
-  "hwchase17",
-  "iakshay",
-  "isahers1",
-  "j-broekhuizen",
-  "jacoblee93",
-  "jdrogers940",
-  "joaquin-borggio-lc",
-  "katmayb",
-  "keshivtandon",
-  "langchain-infra",
-  "lc-arjun",
-  "lc-chad",
-  "lnhsingh",
-  "madams0013",
-  "mdrxy",
-  "mhk197",
-  "nfcampos",
-  "nhuang-lc",
-  "nitboss",
-  "PeriniM",
-  "phvash",
-  "QuentinBrosse",
-  "rlancemartin",
-  "romain-priour-lc",
-  "samecrowder",
-  "samnoyes",
-  "starmorph",
-  "suraj-langchain",
-  "sydney-runkle",
-  "tanushree-sharma",
-  "victorm-lc",
-  "xornivore",
-  "xuro-langchain",
-  "Palashio",
-  "ads2280",
-];
-
 // HACK: Until we setup proper support for API credits, we will only allow users to self host Open SWE
 export function isAllowedUser(username: string): boolean {
+  const nodeEnv = process.env.NODE_ENV;
+  // Allow all users in non-production environments
+  if (nodeEnv !== "production") {
+    return true;
+  }
+
   const restrictToLangChainAuth =
     process.env.RESTRICT_TO_LANGCHAIN_AUTH === "true" ||
     process.env.NEXT_PUBLIC_RESTRICT_TO_LANGCHAIN_AUTH === "true";
   if (!restrictToLangChainAuth) {
     return true;
   }
-  return ALLOWED_USERS.some((u) => u === username);
+
+  let allowedUsers: string[] = [];
+  try {
+    allowedUsers = process.env.NEXT_PUBLIC_ALLOWED_USERS_LIST
+      ? JSON.parse(process.env.NEXT_PUBLIC_ALLOWED_USERS_LIST)
+      : [];
+    if (!allowedUsers.length) {
+      return false;
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Failed to parse allowed users list", error);
+    return false;
+  }
+
+  return allowedUsers.some((u) => u === username);
 }
